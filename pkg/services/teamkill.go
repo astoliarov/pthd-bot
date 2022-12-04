e03086e5494a80172b97e2d7bfa787ead426e5f8
Alexei Stoliarov <alexei.stoliarov@gmail.com>
2022-12-04 14:52:21 +0200

    added test on mix of alphabets validation

--- pkg/services/teamkill.go
+++ pkg/services/teamkill.go
@@ -19,26 +19,6 @@ func NewTeamKillService(dao interfaces.ITeamKillLogDAO, responseSelector *Respon
 	}
 }
 
-func (s *TeamKillService) validateName(name string) error {
-	isCyrSymbolsPresent := false
-	isLatSymbolsPresent := false
-
-	for _, symbol := range name {
-		if unicode.Is(unicode.Cyrillic, symbol) {
-			isCyrSymbolsPresent = true
-		}
-		if unicode.Is(unicode.Latin, symbol) {
-			isLatSymbolsPresent = true
-		}
-	}
-
-	if isCyrSymbolsPresent && isLatSymbolsPresent {
-		return NewErrMixedCharactersInName(name)
-	}
-
-	return nil
-}
-
 func (s *TeamKillService) AddTeamKill(request *TeamKillRequest, source string) (string, error) {
 	if err := s.validateName(request.Killer); err != nil {
 		return "", err
@@ -73,4 +53,24 @@ func (s *TeamKillService) GetTopKillers(source string) ([]*entities.TopKillerLog
 
 func (s *TeamKillService) GetTopVictims(source string) ([]*entities.TopVictimLog, error) {
 	return s.dao.GetTopVictims(source)
+}
+
+func (s *TeamKillService) validateName(name string) error {
+	isCyrSymbolsPresent := false
+	isLatSymbolsPresent := false
+
+	for _, symbol := range name {
+		if unicode.Is(unicode.Cyrillic, symbol) {
+			isCyrSymbolsPresent = true
+		}
+		if unicode.Is(unicode.Latin, symbol) {
+			isLatSymbolsPresent = true
+		}
+	}
+
+	if isCyrSymbolsPresent && isLatSymbolsPresent {
+		return NewErrMixedCharactersInName(name)
+	}
+
+	return nil
 }
